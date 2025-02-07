58696de45449c21a7a8be17f8a746a17edeba576
Tejas Sandeep Khurd <tejkhurd09@gmail.com>
2025-02-07 22:15:20 +0530

    Add files via upload

---
+++ Chapter 1/Simple_Interest_14.java
@@ -0,0 +1,14 @@
+// Write a Java program to calculate the simple interest. 
+
+public class Simple_Interest_14 {
+    public static void main(String[] args) {
+        
+        double Principal = 5000; 
+        double Rate = 5;        
+        int Time = 3;           
+
+        double Simple_Interest = (Principal * Rate * Time) / 100;
+
+        System.out.println(Simple_Interest);
+    }
+}
---
+++ Chapter 1/Sphere_Volume_13.java
@@ -0,0 +1,13 @@
+// Write a Java program to calculate the volume of a sphere. 
+
+public class Sphere_Volume_13 {
+   
+    public static void main(String[] args) {
+        
+        double radius = 7; 
+
+        double volume = (4.0 / 3.0) * 3.14 * radius*radius*radius;
+
+        System.out.println(volume);
+    }
+}
