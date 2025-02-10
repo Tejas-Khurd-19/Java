120478c6e9dd37914ad0f3d75e52e80385bd638b
Tejas Sandeep Khurd <tejkhurd09@gmail.com>
2025-02-10 22:40:36 +0530

    Add files via upload

---
+++ Chapter 2/CharacterDatatype_08.java
@@ -0,0 +1,13 @@
+public class CharacterDatatype_08 {
+    public static void main(String[] args) {
+        char letter = 'A';  // Storing a single character
+        char digit = '5';   // Storing a number as a character
+        char symbol = '#';  // Storing a special symbol
+
+        System.out.println(letter);  // Output: A
+        System.out.println(digit);   // Output: 5
+        System.out.println(symbol);  // Output: #
+    }
+}
+
+
